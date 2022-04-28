6200fae68f2c0962b6c06d8169229aca1428c638
veritascoding <jarrettcoding@gmail.com>
2022-04-28 00:20:02 -0500

    move servier-worker to public and add manifest.json

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const APP_PREFIX = "BudgetTracker-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
-const FILES_TO_CACHE = ["./public/index.html", "./public/assests/style.css"];
+const FILES_TO_CACHE = ["./index.html", "./assests/style.css"];
 
 self.addEventListener("install", function (e) {
   e.waitUntil(
